9a46c3aa66612d401b4e9a7cc460c209098f99b3
wondrousWebWorks <alberts.johann@gmail.com>
2021-04-21 11:52:29 +0000

    Format both files for pep8 compliance

--- test_student.py
+++ test_student.py
@@ -1,7 +1,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
-from unittest.mock import patch
+
 
 class TestStudent(unittest.TestCase):
     """
@@ -19,7 +19,6 @@ class TestStudent(unittest.TestCase):
     def setUpClass(cls):
         print('setUpClass')
 
-
     @classmethod
     def tearDownClass(cls):
         print('tearDownClass')
@@ -28,20 +27,16 @@ class TestStudent(unittest.TestCase):
         print('setUp')
         self.student = Student('John', 'Doe')
 
-
     def tearDown(self):
         print('tearDown')
-
 
     def test_full_name(self):
         print('test_full_name')
         self.assertEqual(self.student.full_name, 'John Doe')
 
-
     def test_email(self):
         print('test_email')
         self.assertEqual(self.student.email, 'john.doe@email.com')
-
 
     def test_alert_santa(self):
         print('test_alert_santa')
@@ -49,27 +44,12 @@ class TestStudent(unittest.TestCase):
 
         self.assertTrue(self.student.naughty_list)
 
-
     def test_apply_extension(self):
         old_end_date = self.student.end_date
         self.student.apply_extension(5)
 
-        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
-
-
-    def test_course_schedule(self):
-        with patch('student.requests.get') as mocked_get:
-            mocked_get.return_value.ok = True
-            mocked_get.return_value.text = 'Success'
-
-            schedule = self.student.course_schedule()
-            self.assertEqual(schedule, 'Success')
-
-            mocked_get.return_value.ok = False
-
-            schedule = self.student.course_schedule()
-            self.assertEqual(schedule, 'Something went wrong with the request')
-
+        self.assertEqual(self.student.end_date,
+                         old_end_date + timedelta(days=5))
 
 
 if __name__ == '__main__':
